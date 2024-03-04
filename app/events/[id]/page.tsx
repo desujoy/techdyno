ff1d09f1928b2dbcb498ce0184dc0ec1cd818cb3
Sujoy De <99689289+desujoy@users.noreply.github.com>
2024-03-04 17:21:15 +0530

    feat: event id pages

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,17 +1,25 @@
 import { ShopItem } from "@/components/definitions/shop-items";
 import ItemDetails from "@/components/ui/ItemDetails";
+import { useRouter } from "next/navigation";
 
 const ShopItems = import("@/app/api/items/route").then(
   (module) => module.ShopItems
 );
 
-export default async function ItemPage({ id }: { id: number }) {
-  console.log(id);
+export default async function ItemPage({
+  params: { id },
+}: {
+  params: { id: number };
+}) {
+  // console.log(id);
   const items: ShopItem[] = await ShopItems;
-  const item = items.find((item) => item.id === id);
+  // console.log(items);
+  const item = items.find((item) => item.id === Number(id));
+  // console.log(item);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ItemDetails item={item!} />
+      {/* <h1>{id}</h1> */}
     </main>
   );
 }
--- components/ui/ItemDetails.tsx
+++ components/ui/ItemDetails.tsx
@@ -5,7 +5,7 @@ export default function ItemDetails({ item }: { item: ShopItem }) {
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold">{item.name}</h1>
-      <Image src={item.image} alt={item.name} />
+      <Image src={item.image} alt={item.name} height={400} width={400}/>
       <p>{item.description}</p>
       <p>${item.price}</p>
     </div>

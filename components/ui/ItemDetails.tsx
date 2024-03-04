fbc98fced5d9e330abf0d0d56d9ff06f3d4a6f73
Sujoy De <99689289+desujoy@users.noreply.github.com>
2024-03-04 19:12:27 +0530

    feat: login

--- components/ui/ItemDetails.tsx
+++ components/ui/ItemDetails.tsx
@@ -8,6 +8,15 @@ export default function eventDetails({ event }: { event: EventItem }) {
       <Image src={event.image} alt={event.name} height={1000} width={1000} />
       <p>{event.description}</p>
       <p>${event.club}</p>
+      <div className="flex flex-col">
+        <h1>Leaderboard</h1>
+        {event.leaderboard.map((leader) => (
+          <div key={leader.id} className="flex flex-row">
+            <p className="flex" key={leader.id}>{leader.name}  </p>
+            <p className="flex" key={leader.id}>{leader.score}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

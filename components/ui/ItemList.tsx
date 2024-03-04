263b28a3877036fd97fbd45f3f2c1b79ecf650f8
Sujoy De <99689289+desujoy@users.noreply.github.com>
2024-03-04 16:14:03 +0530

    feat: horizontal-scroll

--- components/ui/ItemList.tsx
+++ components/ui/ItemList.tsx
@@ -8,39 +8,69 @@ import {
 } from "./card";
 import Image from "next/image";
 import { ShopItem } from "../definitions/shop-items";
+import classes from "./ItemList.module.css";
 
 export default function ItemList({ items }: { items: ShopItem[] }) {
   return (
-    <div className="flex flex-wrap items-center justify-center">
-      {!items && <p>No items found</p>}
-      {items &&
-        items.map((item) => (
-          <Card
-            key={item.id}
-            className="flex flex-col w-full sm:w-1/2 md:w-1/4 lg:w-1/5 xl:w-1/5 m-4"
-          >
-            <CardHeader>
-              <CardTitle>{item.name}</CardTitle>
-              <CardDescription className="text-sm overflow-ellipsis overflow-hidden h-10">
-                {item.description}
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex justify-center items-center">
-              <Image
-                src={item.image}
-                alt={item.name}
-                width={300}
-                height={300}
-              />
-            </CardContent>
-            <CardFooter className="flex justify-between items-center">
-              <p>${item.price}</p>
-              <button className="bg-primary text-primary-foreground rounded-lg p-2">
-                Add to cart
-              </button>
-            </CardFooter>
-          </Card>
-        ))}
-    </div>
+    <>
+      <h1 className="text-3xl font-bold">Upcoming Events</h1>
+      <div className={`${classes['scrolling-wrapper-flexbox']} w-full`}>
+        {!items && <p>No items found</p>}
+        {items &&
+          items.map((item) => (
+            <Card key={item.id} className={`${classes['card']}`}>
+              <CardHeader>
+                <CardTitle>{item.name}</CardTitle>
+                {/* <CardDescription className="text-sm overflow-ellipsis overflow-hidden h-10">
+                  {item.description}
+                </CardDescription> */}
+              </CardHeader>
+              <CardContent className="flex justify-center items-center">
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={300}
+                  height={300}
+                />
+              </CardContent>
+              <CardFooter className="flex justify-between items-center">
+                <p>${item.price}</p>
+                <button className="bg-primary text-primary-foreground rounded-lg p-2">
+                  Add to cart
+                </button>
+              </CardFooter>
+            </Card>
+          ))}
+      </div>
+      <h1 className="text-3xl font-bold">Past Events</h1>
+      <div className={`${classes['scrolling-wrapper-flexbox']} w-full`}>
+        {!items && <p>No items found</p>}
+        {items &&
+          items.map((item) => (
+            <Card key={item.id} className={`${classes['card']}`}>
+              <CardHeader>
+                <CardTitle>{item.name}</CardTitle>
+                {/* <CardDescription className="text-sm overflow-ellipsis overflow-hidden h-10">
+                  {item.description}
+                </CardDescription> */}
+              </CardHeader>
+              <CardContent className="flex justify-center items-center">
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={300}
+                  height={300}
+                />
+              </CardContent>
+              <CardFooter className="flex justify-between items-center">
+                <p>${item.price}</p>
+                <button className="bg-primary text-primary-foreground rounded-lg p-2">
+                  Add to cart
+                </button>
+              </CardFooter>
+            </Card>
+          ))}
+      </div>
+    </>
   );
 }
